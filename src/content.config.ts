5d818600685749b0ee9d3469274172556f96cc99
SurajR2 <surajrasaili190@gmail.com>
2026-04-26 00:52:24 +0545

    nva bar fixes in mobile

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const work = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/work' }),
   schema: z.object({
     title: z.string(),
     summary: z.string(),
@@ -15,7 +16,7 @@ const work = defineCollection({
 });
 
 const blogs = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blogs' }),
   schema: z.object({
     title: z.string(),
     summary: z.string(),
